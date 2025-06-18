ff8b313c105b2300bd9caae16a177ebd37825014
Sapodsoblyaeva <sa.podsoblyaeva@yandex.ru>
2025-06-18 15:30:25 +0300

    feat: graphic

--- lab1.2/lab1.2.py
+++ lab1.2/lab1.2.py
@@ -2,8 +2,6 @@ from matplotlib import pyplot
 from openpyxl import load_workbook
 import os
 
-# print("Текущая рабочая директория:", os.getcwd())
-
 wb = load_workbook('/Users/sveta/Documents/p4ne/lab1.2/data_analysis_lab.xlsx') 
 
 
@@ -19,14 +17,11 @@ list_y = list(map(getvalue, sheet['C'][1:]))
 
 list_z = list(map(getvalue, sheet['D'][1:]))
 
-# print(list_x)
-# print(list_y)
-
-pyplot.plot(list_x, list_y, label="График роста относительной температуры по годам") 
-pyplot.plot(list_x, list_z, label="График роста солнечной активности по годам") 
+pyplot.plot(list_x, list_y, label="Относительная температура") 
+pyplot.plot(list_x, list_z, label="Активность") 
 
 pyplot.xlabel("Годы")
-pyplot.ylabel("Температура и Активность")
+pyplot.ylabel("Показатели")
 pyplot.legend(loc="best")
 
 pyplot.show() 
