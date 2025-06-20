964fadeae1e391d48123887355ed7288605f6d52
Sapodsoblyaeva <sa.podsoblyaeva@yandex.ru>
2025-06-20 18:12:23 +0300

    feat: lab1.4 2.0

--- lab1.4/lab1.4.py
+++ lab1.4/lab1.4.py
@@ -6,6 +6,9 @@ class IPv4RandomNetwork(ipaddress.IPv4Network):
     def __init__(self):
         self.getNewRandomNetwork()
         
+    def __repr__(self):
+        return   "'%s'" % self
+        
     def getNewRandomNetwork(self): 
         self.ip = random.randint(0x0B000000, 0xDF000000)
         self.mask = random.randint(8, 24)
@@ -26,4 +29,18 @@ class IPv4RandomNetwork(ipaddress.IPv4Network):
 net1 = IPv4RandomNetwork()
 print(net1.randomNet())
 
+def value_ip(net): 
+    print(int(net.netmask)*2**32 + int(net.network_address))
+    return int(net.netmask)*2**32 + int(net.network_address)
+    
+    
+
+net_list = []
+for i in range(0, 20): 
+    net = IPv4RandomNetwork()
+    net_list.append(net)
+
+
+new_net_list = sorted(net_list, key=value_ip)
+print(new_net_list)
 
